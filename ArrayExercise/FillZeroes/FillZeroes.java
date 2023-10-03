5c914177b144aec77cb8e40c38748386cd7aec42
Vivek Murarka <113105759+VivekMurarkaIndIre@users.noreply.github.com>
2023-10-03 16:26:03 +0000

    Fill zeroes

--- ArrayExercise/FillZeroes/FillZeroes.java
+++ ArrayExercise/FillZeroes/FillZeroes.java
@@ -7,27 +7,44 @@ public class FillZeroes {
     public static void main(String[] args){
     int[][] matrix= {{3,4,5,6,7},
     {2,0,4,5,8},{3,4,6,0,7}};
-    setZeros(matrix);
+    List<Cell> myList = new ArrayList<>();
+    setZeros(matrix,myList);
       for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                System.out.println(matrix[i][j]);
+                System.out.print(matrix[i][j]);
             }
             System.out.println();
       }
 }
 
-    public static void setZeros(int matrix[][]) {
+    public static void setZeros(int matrix[][],List<Cell> myList ) {
         // Write your code here..
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j] == 0){
-                    Arrays.fill(matrix[i],0);
-                    for(int row=0;row<matrix.length;row++){
-                        matrix[row][j] = 0;
-                    }
+                    Cell cell = new Cell(i,j);
+                    myList.add(cell);
                 }
+            }
+        }
+
+        for(int i=0;i<myList.size();i++){
+            Cell currentCell= myList.get(i);
+            Arrays.fill(matrix[currentCell.row],0);
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][currentCell.col]=0;
             }
         }
     }
 
+
+}
+
+class Cell {
+    int row;
+    int col;
+    public Cell(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
 }
